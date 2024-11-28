1767fb90faf9c95fb36e994fb9c6de616df671c9
xander-haj <114783946+xander-haj@users.noreply.github.com>
2024-11-28 07:06:50 -0600

    Update live_w_locator.js

--- live_w_locator.js
+++ live_w_locator.js
@@ -288,7 +288,45 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             });
         },
-        
+        onProcessed: function (result) {
+            var drawingCtx = Quagga.canvas.ctx.overlay,
+                drawingCanvas = Quagga.canvas.dom.overlay;
+
+            if (result) {
+                if (result.boxes) {
+                    drawingCtx.clearRect(
+                        0,
+                        0,
+                        parseInt(drawingCanvas.getAttribute("width")),
+                        parseInt(drawingCanvas.getAttribute("height"))
+                    );
+                    result.boxes
+                        .filter(function (box) {
+                            return box !== result.box;
+                        })
+                        .forEach(function (box) {
+                            Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+                                color: "green",
+                                lineWidth: 2,
+                            });
+                        });
+                }
+
+                if (result.box) {
+                    Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+                        color: "#00F",
+                        lineWidth: 2,
+                    });
+                }
+
+                if (result.codeResult && result.codeResult.code) {
+                    Quagga.ImageDebug.drawPath(result.line, { x: "x", y: "y" }, drawingCtx, {
+                        color: "red",
+                        lineWidth: 3,
+                    });
+                }
+            }
+        },
         onDetected: function (result) {
             var code = result.codeResult.code;
         
@@ -322,7 +360,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.querySelector("#result_strip ul.thumbnails").prepend(node);
             }
         },
-        
         _printCollectedResults: function () {
             var results = resultCollector.getResults(),
                 ul = document.querySelector("#result_strip ul.collector");
